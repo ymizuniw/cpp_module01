47fde705b296f1c7c789c207cf0e1f5149697b6b
Yugo Mizuniwa <ymizuniw@Yugos-MacBook-Air.local>
2025-11-24 00:54:36 +0900

    error handling for ex00 added.

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -4,19 +4,22 @@ int main(void)
 {
     Zombie obj1("static_zombie");
     Zombie *obj2;
-    try{
-        obj2 = new Zombie("dynamic_zombie");
-    } catch(const std::bad_alloc &e){
-        std::cerr << "memory allocation failed: " << e.what() << std::endl;
+    Zombie *obj3;
+
+    obj2 = newZombie("dynamic_zombie");
+    if (obj2==nullptr)
+    {
+        std::cerr << "memory allocation failed: " << std::endl;
         return (1);
     }
-    Zombie *obj3;
-    obj3 = new(0) Zombie("noexception");
-    if (obj3 == nullptr){
-        std::cerr << "memory allocation failed." << std::endl;
+    obj3 = newZombie("");
+    if (obj3==nullptr)
+    {
+        std::cerr << "memory allocation failed: " << std::endl;
         delete(obj2);
         return (1);
     }
     delete(obj2);
+    delete(obj3);
     return (0);
 }
--- ex00/newZombie.cpp
+++ ex00/newZombie.cpp
@@ -2,6 +2,12 @@
 
 Zombie *newZombie(std::string name)
 {
-    Zombie *new_zombie = new Zombie(name);
+    Zombie *new_zombie = nullptr;
+    try {
+        new_zombie = new Zombie(name);
+    } catch(const std::bad_alloc &e) {
+        std::cerr << "memory allocation failed." << e.what() << std::endl;
+        return (nullptr);
+    }
     return (new_zombie);
 }
