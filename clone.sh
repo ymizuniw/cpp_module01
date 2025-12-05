aaf154f31283d6212775da216999720e5efbdaff
ymizuniw <newerdaysreki@gmail.com>
2025-12-06 00:12:39 +0900

    git_push.sh added.

---
+++ clone.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+git clone git@github.com:ymizuniw/libft_cpp.git
+rm -fr libft_cpp/.git
---
+++ git_push.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+read -p "branch: " BRANCH
+
+if [ $? != 0 ]; then
+    printf '%s\n' "read failed." 
+    exit 1
+else
+    git checkout $BRANCH
+    if [ $? != 0 ]; then
+        printf '%s\n' "checkout failed."
+        exit 1
+    fi
+    git add .
+fi
+
+read -p "commit message: " CMSG
+if [ $? != 0 ]; then
+    printf '%s\n' "read failed."
+    exit 1
+else
+    git commit -m "$CMSG"
+fi
+
+read -p "push?[y/n]: "  PUSH
+if [ $? != 0 ]; then
+    printf '%s\n' "read failed."
+    exit 1
+else
+    git push "$BRANCH"
+fi
