71aaff1a388ee255d5d57073f8bf7e240bd124ff
Yugo Mizuniwa <ymizuniw@Yugos-MacBook-Air.local>
2025-12-05 21:48:16 +0900

    ex01:make test written

--- ex01/test.sh
+++ ex01/test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-make
+make > /dev/null
+if [ $? != 0 ]; then
+    exit 1
+fi
 
 read -p "Zombie num : " N_ZOMB
 if [ $? != 0 ]; then
@@ -12,7 +15,7 @@ if [ $? != 0 ]; then
     exit 1
 fi
 
-./test << EOF
+./test01 << EOF
 $N_ZOMB
 $NAME_ZOMB
 EOF
