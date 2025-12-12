79e0522e1640f636659f61b11c12e9fe9e83bfa7
Yugo Mizuniwa <ymizuniw@c6r2s14.42tokyo.jp>
2025-12-12 18:20:02 +0900

    test suit updated

--- test.sh
+++ test.sh
@@ -1,56 +1,56 @@
 #!/bin/bash
 
-printf '%-10s\n' "ex00"
+printf '\n=====%s=====\n' "ex00"
 cd ex00/ && make && ./test00
-if [ $? != 0]; then
+if [ $? != 0 ]; then
     echo "fail"
     exit 1
 else
     echo "success"
 fi
-printf '%-10s\n' "ex01"
+printf '\n=====%s=====\n' "ex01"
 cd ../ex01 && make && ./test01
-if [ $? != 0]; then
+if [ $? != 0 ]; then
     echo "fail"
     exit 1
 else
     echo "success"
 fi
-printf '%-10s\n' "ex02"
+printf '\n=====%s=====\n' "ex02"
 cd ../ex02 && make && ./test02
-if [ $? != 0]; then
+if [ $? != 0 ]; then
     echo "fail"
     exit 1
 else
     echo "success"
 fi
-printf '%-10s\n' "ex03"
+printf '\n=====%s=====\n' "ex03"
 cd ../ex03 && make && ./test03
-if [ $? != 0]; then
+if [ $? != 0 ]; then
     echo "fail"
     exit 1
 else
     echo "success"
 fi
-printf '%-10s\n' "ex04"
+printf '\n=====%s=====\n' "ex04"
 cd ../ex04 && make && ./test04.sh
-if [ $? != 0]; then
+if [ $? != 0 ]; then
     echo "fail"
     exit 1
 else
     echo "success"
 fi
-printf '%-10s\n' "ex05"
+printf '\n=====%s=====\n' "ex05"
 cd ../ex05 && make && ./test05 <<< INFO
-if [ $? != 0]; then
+if [ $? != 0 ]; then
     echo "fail"
     exit 1
 else
     echo "success"
 fi
-printf '%-10s\n' "ex06"
+printf '\n=====%s=====\n' "ex06"
 cd ../ex06 && make && ./test06 WARNING
-if [ $? != 0]; then
+if [ $? != 0 ]; then
     echo "fail"
     exit 1
 else
